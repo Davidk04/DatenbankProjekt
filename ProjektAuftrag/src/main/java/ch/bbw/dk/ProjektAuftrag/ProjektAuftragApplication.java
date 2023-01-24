1978e76487e44e869025e4f397ef31b2ffb99f01
davidk04 <david.kresic@swisscom.com>
2023-01-24 22:37:42 +0100

    initial push

---
+++ ProjektAuftrag/src/main/java/ch/bbw/dk/ProjektAuftrag/ProjektAuftragApplication.java
@@ -0,0 +1,13 @@
+package ch.bbw.dk.ProjektAuftrag;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjektAuftragApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjektAuftragApplication.class, args);
+	}
+
+}
---
+++ ProjektAuftrag/src/test/java/ch/bbw/dk/ProjektAuftrag/ProjektAuftragApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.bbw.dk.ProjektAuftrag;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjektAuftragApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
