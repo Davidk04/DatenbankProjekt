2dbec80c5fd163e2cfc7aa305c100592bd8dbcef
taakrdab <david.kresic@swisscom.com>
2023-01-25 19:19:32 +0100

    added application.properties for db connection

--- ProjektAuftrag/src/main/java/ch/bbw/dk/ProjektAuftrag/service/MemberService.java
+++ ProjektAuftrag/src/main/java/ch/bbw/dk/ProjektAuftrag/service/MemberService.java
@@ -3,11 +3,10 @@ package ch.bbw.dk.ProjektAuftrag.service;
 import ch.bbw.dk.ProjektAuftrag.model.Member;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 public class MemberService {
 
-
-    public Member getMember(Integer id){
-        return null;
-    }
 }
